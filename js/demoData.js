768367e681557eeb6cf688f638eaf9da35aaf67e
Bayarchimegch <bayarchimegchimed@gmail.com>
2025-01-02 23:42:02 +0800

    Create demoData.js

---
+++ js/demoData.js
@@ -0,0 +1,97 @@
+export const movies = [
+    {
+      id: 1,
+      title: "Sonic the Hedgehog 3",
+      description: "The fast and adventurous Sonic returns to save the world from a new threat.",
+      releaseYear: 2024,
+      genre: ["Animation", "Adventure", "Comedy"],
+      posterUrl: "images/moviePoster/sonic-hedgehog-3.jpg",
+      rating: 8.2
+    },
+    {
+      id: 2,
+      title: "Smile 2",
+      description: "A chilling sequel that continues the horrifying tale of mysterious smiles.",
+      releaseYear: 2025,
+      genre: ["Horror", "Thriller"],
+      posterUrl: "images/moviePoster/smile-2.jpg",
+      rating: 7.5
+    },
+    {
+      id: 3,
+      title: "The Truman Show",
+      description: "An ordinary man discovers his entire life has been a reality TV show.",
+      releaseYear: 1998,
+      genre: ["Drama", "Comedy"],
+      posterUrl: "images/moviePoster/truman-show.jpg",
+      rating: 8.1
+    },
+    {
+      id: 4,
+      title: "Forrest Gump",
+      description: "The story of a kind-hearted man who witnesses key historical events.",
+      releaseYear: 1994,
+      genre: ["Drama", "Romance"],
+      posterUrl: "images/moviePoster/forest-gump.jpg",
+      rating: 8.8
+    }
+  ];
+  
+  export const moviesList = [
+    {
+      id: 1,
+      title: "Sonic the Hedgehog 3",
+      description: "The fast and adventurous Sonic returns to save the world from a new threat.",
+      releaseYear: 2024,
+      genre: ["Animation", "Adventure", "Comedy"],
+      posterUrl: "images/moviePoster/sonic-hedgehog-3.jpg",
+      rating: 8.2
+    },
+    {
+      id: 2,
+      title: "Smile 2",
+      description: "A chilling sequel that continues the horrifying tale of mysterious smiles.",
+      releaseYear: 2025,
+      genre: ["Horror", "Thriller"],
+      posterUrl: "images/moviePoster/smile-2.jpg",
+      rating: 7.5
+    },
+    {
+      id: 3,
+      title: "The Truman Show",
+      description: "An ordinary man discovers his entire life has been a reality TV show.",
+      releaseYear: 1998,
+      genre: ["Drama", "Comedy"],
+      posterUrl: "images/moviePoster/truman-show.jpg",
+      rating: 8.1
+    },
+    {
+      id: 4,
+      title: "Forrest Gump",
+      description: "The story of a kind-hearted man who witnesses key historical events.",
+      releaseYear: 1994,
+      genre: ["Drama", "Romance"],
+      posterUrl: "images/moviePoster/forest-gump.jpg",
+      rating: 8.8
+    }, 
+    {
+      id: 5,
+      title: "Sonic the Hedgehog 3",
+      description: "The fast and adventurous Sonic returns to save the world from a new threat.",
+      releaseYear: 2024,
+      genre: ["Animation", "Adventure", "Comedy"],
+      posterUrl: "images/moviePoster/sonic-hedgehog-3.jpg",
+      rating: 8.2
+    },
+    {
+      id: 6,
+      title: "Smile 2",
+      description: "A chilling sequel that continues the horrifying tale of mysterious smiles.",
+      releaseYear: 2025,
+      genre: ["Horror", "Thriller"],
+      posterUrl: "images/moviePoster/smile-2.jpg",
+      rating: 7.5
+    },
+  ];
+  
+  
